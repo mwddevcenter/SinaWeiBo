7e079466022561b86475cfb00427ac65071a2f9d
steveguoshao <steveguoshao@gmail.com>
2012-05-28 23:37:44 +0800

    11

---
+++ SinaWeiBo/src/com/powerdata/android/weibo/LogonActivity.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+package com.powerdata.android.weibo;
+
+/**
+ * @fileName LogonActivity.java
+ * @author SteveGuo
+ * @createDate 2012-5-28 下午11:37:25
+ */
+public class LogonActivity {
+
+}
